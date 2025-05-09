347a09931ddbf522dc73d1c7d7e63c3203d1e498
alireza barkhordari <alirezab.1990@gmail.com>
2025-05-09 19:11:45 +0330

    Refactor CreateLearningJourneyRequest class
    Updated the `CreateLearningJourneyRequest` class to be sealed and added new properties: `UserId`, `LearningLanguageId`, `LearningTarget`, `PracticePerDayInMinutes`, and proficiency levels for reading, writing, listening, and speaking.

--- Src/01.Core/LanguageLearning.Core.Application/LearningJourney/Commands/CreateLearningJourney/DTO/CreateLearningJourneyRequest.cs
+++ Src/01.Core/LanguageLearning.Core.Application/LearningJourney/Commands/CreateLearningJourney/DTO/CreateLearningJourneyRequest.cs
@@ -1,4 +1,15 @@
-﻿namespace LanguageLearning.Core.Application.LearningJourney.Commands.CreateLearningJourney.DTO;
-public class CreateLearningJourneyRequest
+﻿using LanguageLearning.Core.Domain.LearningJourney.Enums;
+
+namespace LanguageLearning.Core.Application.LearningJourney.Commands.CreateLearningJourney.DTO;
+public sealed class CreateLearningJourneyRequest
 {
+    public long UserId { get; set; }
+    public int LearningLanguageId { get; set; }
+    public LearningTarget LearningTarget { get; private set; }
+    public int PracticePerDayInMinutes { get; private set; }
+    public ProficiencyLevel ReadingProficiency { get; set; }
+    public ProficiencyLevel WritingProficiency { get; set; }
+    public ProficiencyLevel ListeningProficiency { get; set; }
+    public ProficiencyLevel SpeakingProficiency { get; set; }    
+
 }
