cd4013d5cc6e7856f6edc37081cf0a9cad0ac3bc
alireza barkhordari <alirezab.1990@gmail.com>
2025-05-22 19:03:37 +0330

    Add LearningPathAddedEvent for journey updates
    Introduces a new record type `LearningPathAddedEvent`
    that implements the `IDomainEvent` interface. This event
    captures the addition of a learning path with a
    `JourneyId` parameter of type `long`. Also adds a
    using directive for `LanguageLearning.Core.Domain.Framework.Events`.

---
+++ Src/01.Core/LanguageLearning.Core.Domain/LearningJourneys/Events/LearningPathAddedEvent.cs
@@ -0,0 +1,4 @@
+﻿using LanguageLearning.Core.Domain.Framework.Events;
+
+namespace LanguageLearning.Core.Domain.LearningJourneys.Events;
+public sealed record LearningPathAddedEvent(long JourneyId) : IDomainEvent;
