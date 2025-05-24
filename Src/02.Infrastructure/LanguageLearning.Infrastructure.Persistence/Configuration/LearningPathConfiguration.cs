88d0cb89f1a3f937c7877f19d2767badf58567f1
alireza barkhordari <alirezab.1990@gmail.com>
2025-05-24 20:45:26 +0330

    Remove LearningGoal configuration; add LearningPath config
    The `LearningGoalConfiguration.cs` file has been removed, eliminating the configuration for the `LearningGoal` entity.
    
    A new `LearningPathConfiguration.cs` file has been added, introducing the configuration for the `LearningPath` entity. This includes specifications for the table name, primary key, required properties, and property configurations for `CreatedDate`, `Status`, `TotalSessions`, and `RowVersion`. It also ignores certain properties and establishes a one-to-many relationship with the `LearningSession` entity, implementing cascade delete behavior.

---
+++ Src/02.Infrastructure/LanguageLearning.Infrastructure.Persistence/Configuration/LearningPathConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using LanguageLearning.Core.Domain.LearningJourneys.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LanguageLearning.Infrastructure.Persistence.Configuration;
+public sealed class LearningPathConfiguration : IEntityTypeConfiguration<LearningPath>
+{
+    public void Configure(EntityTypeBuilder<LearningPath> builder)
+    {
+        builder.ToTable("LearningGoals");
+
+        builder.HasKey(lg => lg.Id);
+
+        builder.Property(lg => lg.CreatedDate)
+            .IsRequired();
+
+        builder.Property(lg => lg.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(lg => lg.TotalSessions)
+            .IsRequired();
+
+        builder.Property(lg => lg.RowVersion)
+            .IsRowVersion();
+
+        builder.Ignore(lg => lg.CompletedSessions);
+        builder.Ignore(lg => lg.Sessions);
+        builder.Ignore(lg => lg.IsCompleted);
+
+        builder
+            .HasMany<LearningSession>("_sessions")
+            .WithOne()
+            .HasForeignKey("LearningPathId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
