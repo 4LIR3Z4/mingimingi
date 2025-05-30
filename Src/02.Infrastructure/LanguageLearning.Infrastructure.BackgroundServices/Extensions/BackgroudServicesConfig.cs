1f78f8deb5ea21aa65fba327780e1d0b7924e991
alireza barkhordari <alirezab.1990@gmail.com>
2025-05-30 09:01:46 -0700

    Add BackgroundServicesConfig for hosted service registration
    Introduced a new static class `BackgroudServicesConfig` in the
    `LanguageLearning.Infrastructure.BackgroundServices.Extensions` namespace.
    This class contains the `ConfigureBackgroudServices` method, which
    registers the `JourneyProcessingService` as a hosted service in the
    dependency injection container.

---
+++ Src/02.Infrastructure/LanguageLearning.Infrastructure.BackgroundServices/Extensions/BackgroudServicesConfig.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LanguageLearning.Infrastructure.BackgroundServices.Extensions;
+public static class BackgroudServicesConfig
+{
+    public static void ConfigureBackgroudServices(this IServiceCollection services)
+    {
+        services.AddHostedService<JourneyProcessingService>();
+    }
+}
