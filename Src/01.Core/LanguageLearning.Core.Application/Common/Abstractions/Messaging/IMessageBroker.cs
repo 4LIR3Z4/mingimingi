5c63b319670ab45db8e5268bd346fceb82b49822
alireza barkhordari <alirezab.1990@gmail.com>
2025-06-12 16:33:53 +0330

    Refactor IMessageBroker method signatures
    Updated `PublishMessageAsync` to remove routing key and
    modified `SubscribeToQueueAsync` to accept an exchange name
    and a cancellation token, improving consistency and functionality.

--- Src/01.Core/LanguageLearning.Core.Application/Common/Abstractions/Messaging/IMessageBroker.cs
+++ Src/01.Core/LanguageLearning.Core.Application/Common/Abstractions/Messaging/IMessageBroker.cs
@@ -1,6 +1,6 @@
 ﻿namespace LanguageLearning.Core.Application.Common.Abstractions.Messaging;
 public interface IMessageBroker
 {
-    public Task PublishMessageAsync<T>(T message, string exchangeName, string routingKey, CancellationToken cancellationToken);
-    public Task<string> SubscribeToQueueAsync<T>(string queueName, Func<T, Task> handler);
+    public Task PublishMessageAsync<T>(T message, string exchangeName, CancellationToken cancellationToken);
+    public Task<string> SubscribeToQueueAsync<T>(string exchangeName, Func<T, Task> handler, CancellationToken cancellationToken);
 }
