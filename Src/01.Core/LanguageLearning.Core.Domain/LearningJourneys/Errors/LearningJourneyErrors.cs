937e3783e734be16f8d30a64fd34b121360dbba1
alireza barkhordari <alirezab.1990@gmail.com>
2025-06-13 23:49:09 +0330

    Add LearningJourneyErrors class for error handling
    Introduced a new static class `LearningJourneyErrors` in the
    `LanguageLearning.Core.Domain.LearningJourneys.Errors` namespace.
    This class includes two static readonly properties:
    `LanguageNotFound` and `UserNotFound`, providing specific error
    messages for scenarios where a language or user cannot be found.

---
+++ Src/01.Core/LanguageLearning.Core.Domain/LearningJourneys/Errors/LearningJourneyErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace LanguageLearning.Core.Domain.LearningJourneys.Errors;
+public static class LearningJourneyErrors
+{
+    public static readonly Error LanguageNotFound = new Error("Language.NotFound", "The specified target language does not exist.");
+    public static readonly Error UserNotFound = new Error("User.NotFound", "User not found.");
+
+}
