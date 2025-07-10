9ed22da3596fc4e93775736126426ba24bdc683e
alireza barkhordari <alirezab.1990@gmail.com>
2025-07-11 00:16:24 +0330

    Refactor MessageBroker for improved queue handling
    Updated the MessageBroker class to accept IConnection, IChannel, and MessageBrokerSettings as constructor parameters. Replaced QueueName with Queue in PublishMessageAsync and SubscribeToQueueAsync methods. Introduced DeclareTopologyAsync for exchange and queue declarations. Enhanced error handling in message processing and cleaned up commented-out code.

--- Src/02.Infrastructure/LanguageLearning.Infrastructure.Messaging/RabbitMQ/MessageBroker.cs
+++ Src/02.Infrastructure/LanguageLearning.Infrastructure.Messaging/RabbitMQ/MessageBroker.cs
@@ -11,8 +11,7 @@ public sealed class MessageBroker : IMessageBroker
     private IConnection _connection;
     private IChannel _channel;
     private MessageBrokerSettings _settings;
-    private const string ExchangeName = "MainExchange";
-    
+
     public MessageBroker(IConnection connection, IChannel channel, MessageBrokerSettings settings)
     {
         _connection = connection;
@@ -20,15 +19,59 @@ public sealed class MessageBroker : IMessageBroker
         _settings = settings;
     }
 
-    public async Task PublishMessageAsync<T>(T message, QueueName queue, CancellationToken cancellationToken)
+    public async Task PublishMessageAsync<T>(T message, Queue queue, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        await _channel.ExchangeDeclareAsync(exchange: ExchangeName, type: ExchangeType.Fanout, durable: true, cancellationToken: cancellationToken);
-
-
         var body = JsonSerializer.SerializeToUtf8Bytes(message);
 
-        await _channel.BasicPublishAsync(ExchangeName, string.Empty, body, cancellationToken);
+        await _channel.BasicPublishAsync(queue.Exchange.Name, queue.Name, body, cancellationToken);
+
+        await Task.CompletedTask;
+    }
+    public async static Task DeclareTopologyAsync(
+        IChannel channel,
+        List<Queue> queues,
+        CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var exchangeGroups = queues
+            .GroupBy(q => q.Exchange)
+            .ToList();
+
+        foreach (var exchangeGroup in exchangeGroups)
+        {
+            var exchange = exchangeGroup.Key;
+
+            await channel.ExchangeDeclareAsync(
+                exchange: exchange.Name,
+                type: exchange.Type.ToString().ToLower(),
+                durable: exchange.Durable,
+                autoDelete: exchange.AutoDelete,
+                arguments: exchange.Arguments,
+                cancellationToken: cancellationToken);
+
+            foreach (var queue in exchangeGroup)
+            {
+                await channel.QueueDeclareAsync(
+                    queue: queue.Name,
+                    durable: queue.Durable,
+                    exclusive: queue.Exclusive,
+                    autoDelete: queue.AutoDelete,
+                    arguments: queue.Arguments,
+                    cancellationToken: cancellationToken);
+
+
+                await channel.QueueBindAsync(
+                    queue: queue.Name,
+                    exchange: exchange.Name,
+                    routingKey: queue.Name,
+                    cancellationToken: cancellationToken);
+
+
+
+            }
+        }
 
         await Task.CompletedTask;
     }
@@ -45,32 +88,46 @@ public sealed class MessageBroker : IMessageBroker
 
         var connection = await factory.CreateConnectionAsync(cancellationToken);
         var channel = await connection.CreateChannelAsync(cancellationToken: cancellationToken);
-        await channel.ExchangeDeclareAsync(exchange: ExchangeName, type: ExchangeType.Fanout, durable: true, cancellationToken: cancellationToken);
-        foreach (var queue in Enum.GetValues(typeof(QueueName)).Cast<QueueName>())
+        await DeclareTopologyAsync(channel, QueueConfig.GetAllQueues(), cancellationToken);
+
+        /*await channel.ExchangeDeclareAsync(exchange: ExchangeName, type: ExchangeType.Direct, durable: true, cancellationToken: cancellationToken);
+        foreach (var queue in Enum.GetValues(typeof(Queue)).Cast<Queue>())
         {
             QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync(queue: queue.ToString(), durable: true, exclusive: false, autoDelete: false, cancellationToken: cancellationToken);
-            await channel.QueueBindAsync(queue: queue.ToString(), exchange: ExchangeName, routingKey: string.Empty, cancellationToken: cancellationToken);
-        }       
+            await channel.QueueBindAsync(queue: queue.ToString(), exchange: ExchangeName, routingKey: queue.ToString(), cancellationToken: cancellationToken);
+        }*/
         return new MessageBroker(connection, channel, settings);
     }
-    public async Task<string> SubscribeToQueueAsync<T>(QueueName queue, Func<T, Task> handler, CancellationToken cancellationToken)
+    public async Task SubscribeToQueueAsync<T>(Queue queue, Func<T, Task> handler, CancellationToken cancellationToken)
     {
         var consumer = new AsyncEventingBasicConsumer(_channel);
         string message = string.Empty;
-        consumer.ReceivedAsync += (model, ea) =>
+        consumer.ReceivedAsync += async (model, ea) =>
         {
             cancellationToken.ThrowIfCancellationRequested();
             var body = ea.Body.ToArray();
             message = Encoding.UTF8.GetString(body);
-            var deserializedMessage = JsonSerializer.Deserialize<T>(message);
-            if (deserializedMessage is not null)
-                return handler(deserializedMessage);
+            try
+            {
+                var deserializedMessage = JsonSerializer.Deserialize<T>(message);
+                if (deserializedMessage is not null)
+                {
+                    await handler(deserializedMessage);
+                    await _channel.BasicAckAsync(ea.DeliveryTag, false, cancellationToken);
+                    return;
 
-            return Task.CompletedTask;
+                }
+                await _channel.BasicRejectAsync(ea.DeliveryTag, false, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                await _channel.BasicRejectAsync(ea.DeliveryTag, false, cancellationToken);
+            }
+            return;
         };
 
-        await _channel.BasicConsumeAsync(queue.ToString(), autoAck: true, consumer: consumer);
-        return await Task.FromResult(string.Empty);
+        await _channel.BasicConsumeAsync(queue.Name, autoAck: false, consumer: consumer, cancellationToken);
+        await Task.CompletedTask;
 
     }
 }
