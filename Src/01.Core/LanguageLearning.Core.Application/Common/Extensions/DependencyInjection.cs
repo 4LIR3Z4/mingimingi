5ef5421853f95f5fff79cc19c410787daabfa8aa
alireza barkhordari <alirezab.1990@gmail.com>
2025-07-10 23:38:03 +0330

    Add learning journey and path command handlers
    This commit introduces new using directives for `CreateLearningJourney` and `CreateLearningPath` commands and their DTOs in `DependencyInjection.cs`. Additionally, it registers the command handlers for these new commands, enabling dependency injection for their functionalities.

--- Src/01.Core/LanguageLearning.Core.Application/Common/Extensions/DependencyInjection.cs
+++ Src/01.Core/LanguageLearning.Core.Application/Common/Extensions/DependencyInjection.cs
@@ -2,6 +2,10 @@
 using LanguageLearning.Core.Application.Common.Framework;
 using LanguageLearning.Core.Application.Common.Framework.MediatorWrappers.Commands;
 using LanguageLearning.Core.Application.Common.Framework.MediatorWrappers.Queries;
+using LanguageLearning.Core.Application.LearningJourneys.Commands.CreateLearningJourney;
+using LanguageLearning.Core.Application.LearningJourneys.Commands.CreateLearningJourney.DTO;
+using LanguageLearning.Core.Application.LearningJourneys.Commands.CreateLearningPath;
+using LanguageLearning.Core.Application.LearningJourneys.Commands.CreateLearningPath.DTO;
 using LanguageLearning.Core.Application.UserProfiles.Commands.CreateUserProfile;
 using LanguageLearning.Core.Application.UserProfiles.Commands.CreateUserProfile.DTO;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +29,8 @@ public static class DependencyInjection
         });
 
         services.AddTransient<ICommandHandler<CreateUserProfileCommand, CreateProfileResponse>, CreateUserProfileCommandHandler>();
+        services.AddTransient<ICommandHandler<CreateLearningJourneyCommand, CreateLearningJourneyResponse>, CreateLearningJourneyCommandHandler>();
+        services.AddTransient<ICommandHandler<CreateLearningPathCommand, CreateLearningPathResponse>, CreateLearningPathCommandHandler>();
 
         services.AddValidatorsFromAssemblyContaining<IIdGenerator>(includeInternalTypes: true);
 
